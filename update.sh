3a88e068ec7cc5ba0052ce3521e65411c07750e5
AGPSN <github@agpsn.com>
2023-04-22 08:54:31 -0500

    Updated

--- update.sh
+++ update.sh
@@ -2,10 +2,12 @@
 set -eu
 
 echo $(cat ~/.ghcr-token) | docker login ghcr.io -u $(cat ~/.ghcr-user) --password-stdin &>/dev/null
-GBRANCH=$(git branch | grep "*" | rev | cut -f1 -d" " | rev)
+
+#GBRANCH=$(git branch | grep "*" | rev | cut -f1 -d" " | rev)
+
 SVERSION=$(curl -sX GET http://services.sonarr.tv/v1/releases | jq -r "first(.[] | select(.branch==\"develop\") | .version)");
 
-if [ $GBRANCH != "develop" ]; then git checkout "develop"; fi
+#if [ $GBRANCH != "develop" ]; then git checkout "develop"; fi
 	echo "Building and Pushing 'ghcr.io/agpsn/docker-sonarr:$SVERSION'"
 	docker build --quiet --force-rm --rm --tag ghcr.io/agpsn/docker-sonarr:develop --tag ghcr.io/agpsn/docker-sonarr:${SVERSION} --tag ghcr.io/agpsn/docker-sonarr:latest -f ./Dockerfile.develop .
 	docker push --quiet ghcr.io/agpsn/docker-sonarr:develop; docker push --quiet ghcr.io/agpsn/docker-sonarr:$SVERSION && docker image rm -f ghcr.io/agpsn/docker-sonarr:$SVERSION
