6415341e3b55b59612804fa2e13d70241073a611
AGPSN <github@agpsn.com>
2023-04-30 20:46:21 -0500

    Updated

--- update.sh
+++ update.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -eu
 
-echo $(cat ~/.ghcr-token) | docker login ghcr.io -u $(cat ~/.ghcr-user) --password-stdin &>/dev/null
+[ ! -d "/mnt/user/system/agpsn-github/sonarr-develop" ] && echo "No repo!" && exit 1
+cd "/mnt/user/system/agpsn-github/sonarr-develop"
 
-#GBRANCH=$(git branch | grep "*" | rev | cut -f1 -d" " | rev)
+echo $(cat ~/.ghcr-token) | docker login ghcr.io -u $(cat ~/.ghcr-user) --password-stdin &>/dev/null
 
 SVERSION=$(curl -sX GET http://services.sonarr.tv/v1/releases | jq -r "first(.[] | select(.branch==\"develop\") | .version)");
 
-#if [ $GBRANCH != "develop" ]; then git checkout "develop"; fi
-	echo "Building and Pushing 'ghcr.io/agpsn/docker-sonarr:$SVERSION'"
-	docker build  --force-rm --rm --tag ghcr.io/agpsn/docker-sonarr:develop --tag ghcr.io/agpsn/docker-sonarr:${SVERSION} --tag ghcr.io/agpsn/docker-sonarr:latest -f ./Dockerfile.develop .
-	docker push  ghcr.io/agpsn/docker-sonarr:develop; docker push  ghcr.io/agpsn/docker-sonarr:$SVERSION && docker image rm -f ghcr.io/agpsn/docker-sonarr:$SVERSION
-	git tag -f $SVERSION && git push origin $SVERSION -f --tags
-	git add . && git commit -m "Updated" && git push 
+echo "Building and Pushing 'ghcr.io/agpsn/docker-sonarr:$SVERSION'"
+docker build  --force-rm --rm --tag ghcr.io/agpsn/docker-sonarr:develop --tag ghcr.io/agpsn/docker-sonarr:${SVERSION} --tag ghcr.io/agpsn/docker-sonarr:latest -f ./Dockerfile.develop .
+docker push --quiet ghcr.io/agpsn/docker-sonarr:develop; docker push --quiet ghcr.io/agpsn/docker-sonarr:$SVERSION && docker image rm -f ghcr.io/agpsn/docker-sonarr:$SVERSION
+git tag -f $SVERSION && git push --quiet origin $SVERSION -f --tags
+git add . && git commit -m "Updated" && git push --quiet
+echo ""
