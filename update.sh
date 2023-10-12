1f015d21903ac1154198ef63c511422060119c03
AGPSN <101997295+AGPSN-BOT@users.noreply.github.com>
2023-10-12 06:24:11 -0700

    Updated

--- update.sh
+++ update.sh
@@ -3,8 +3,6 @@ set -eu
 
 SVERSION=$(curl -sX GET http://services.sonarr.tv/v1/releases | jq -r "first(.[] | select(.branch==\"develop\") | .version)");
 
-echo $(cat ../.token) | docker login ghcr.io -u $(cat ../.user) --password-stdin &>/dev/null
-
 echo "Updating Sonarr: v$SVERSION"
 docker build --quiet --force-rm --rm --tag ghcr.io/agpsn/docker-sonarr:develop --tag ghcr.io/agpsn/docker-sonarr:$SVERSION -f ./Dockerfile.develop .
 git tag -f $SVERSION && git push --quiet origin $SVERSION -f --tags
